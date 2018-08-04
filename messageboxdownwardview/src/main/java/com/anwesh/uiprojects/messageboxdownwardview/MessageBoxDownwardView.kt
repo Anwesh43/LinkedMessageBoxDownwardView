baa52ff77cdd26ddcc7c1a1e53f325a2d9974dcc
Anwesh43 <anweshthecool0@gmail.com>
2018-08-05 04:39:24 +0530

    adding LinkedMBD class

--- messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
+++ messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
@@ -145,4 +145,28 @@ class MessageBoxDownwardView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedMBD(var i : Int) {
+
+        private var curr : MBDNode = MBDNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
