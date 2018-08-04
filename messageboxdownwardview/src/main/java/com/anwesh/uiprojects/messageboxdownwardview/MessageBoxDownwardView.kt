27aa6802b35fab340f48b10df7363855d039b235
Anwesh43 <anweshthecool0@gmail.com>
2018-08-05 04:23:08 +0530

    adding MessageBoxDownwardView class

--- messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
+++ messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
@@ -37,3 +37,21 @@ fun Canvas.drawMBDNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class MessageBoxDownwardView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
