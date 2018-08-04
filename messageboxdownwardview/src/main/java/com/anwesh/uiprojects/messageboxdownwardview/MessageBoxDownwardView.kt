271010f23cefdcf5045fc18925074b60f335ad36
Anwesh43 <anweshthecool0@gmail.com>
2018-08-05 04:11:38 +0530

    importing libraries and defining number of nodes

--- messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
+++ messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.messageboxdownwardview
 /**
  * Created by anweshmishra on 05/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
+
+val nodes : Int = 5
+
