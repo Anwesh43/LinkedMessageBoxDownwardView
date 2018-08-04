82154fb1841719e8f84914db4091e1f39f004b88
Anwesh43 <anweshthecool0@gmail.com>
2018-08-05 04:33:39 +0530

    adding MBDNode class

--- messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
+++ messageboxdownwardview/src/main/java/com/anwesh/uiprojects/messageboxdownwardview/MessageBoxDownwardView.kt
@@ -102,4 +102,47 @@ class MessageBoxDownwardView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MBDNode(var i : Int, val state : State = State()) {
+
+        private var next : MBDNode? = null
+
+        private var prev : MBDNode? = null
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMBDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        init {
+            addNeighbor()
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MBDNode {
+            var curr : MBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
